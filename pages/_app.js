681d0b4bb99d6ec607a6252d116a3e6761f1d02c
ABSanthosh <a.b.santhosh02@gmail.com>
2023-03-05 16:20:01 +0530

    feat: added meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -18,7 +18,10 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>Glitch </title>
-        <meta name="description" content="Awesome website for Glitch hack" />
+        <meta
+          name="description"
+          content="Introducing GLITCH, the flagship hackathon and a collaboration between the Google Developer Student Clubs (GDSC) and the ACM & ACM-W chapters of Shiv Nadar Institution of Eminence. Participate to seize the opportunity to learn, collaborate and build meaningful projects alongside mentors from various disciplines."
+        />
         <link rel="icon" href="/Images/glitchLogo.svg" />
       </Head>
       <Component {...pageProps} />
